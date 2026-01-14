3af028242e1dc3db19f064cc2335698203d075ef
Santiago Vázquez <svazgar0106@g.educaand.es>
2026-01-14 18:07:39 +0100

    Inicio proyecto

---
+++ app/src/main/java/com/example/ejercicio22tiendavirtual/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.ejercicio22tiendavirtual
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ejercicio22tiendavirtual.pantallas.PantallaHome
+import com.example.ejercicio22tiendavirtual.ui.theme.Ejercicio22TiendaVirtualTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ejercicio22TiendaVirtualTheme {
+                PantallaHome()
+            }
+        }
+    }
+}
