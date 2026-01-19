2d85fabf10f7919ef1ca5b72504179d79365f350
Santiago Vázquez <svazgar0106@g.educaand.es>
2026-01-19 01:56:53 +0100

    Terminado

--- app/src/main/java/com/example/ejercicio22tiendavirtual/pantallas/PantallaDetalle.kt
+++ app/src/main/java/com/example/ejercicio22tiendavirtual/pantallas/PantallaDetalle.kt
@@ -10,15 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
